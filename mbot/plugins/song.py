3176b47d2e6a964be055644a2fc97de184c8d0e4
Arshadkn <93204996+Arshadkn@users.noreply.github.com>
2023-08-01 22:05:50 +0530

    Create song.py

---
+++ mbot/plugins/song.py
@@ -0,0 +1,94 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.errors import ChatWriteForbidden
+import yt_dlp
+from youtube_search import YoutubeSearch
+import requests
+import wget
+import hashlib 
+import os
+
+@Client.on_message(filters.command(['song']) & filters.group)
+async def song_fetch(client, message):
+    query = ''
+    for i in message.command[1:]:
+        query += ' ' + str(i)
+    print(query)
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    try:
+        results = []
+        count = 0
+        while len(results) == 0 and count < 6:
+            if count > 0:
+                time.sleep(1)
+            results = YoutubeSearch(query, max_results=1).to_dict()
+            count += 1
+        # results = YoutubeSearch(query, max_results=1).to_dict()
+        try:
+            link = f"https://youtube.com{results[0]['url_suffix']}"
+            # print(results)
+            title = results[0]["title"]
+            thumbnail = results[0]["thumbnails"][0]
+            duration = results[0]["duration"]
+            views = results[0]["views"]
+
+            ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
+            # if time_to_seconds(duration) >= 7000:  # duration limit
+            #     m.edit("Exceeded 30mins cap")
+            #     return
+
+            performer = f"[@AnnabenbotZ]"
+            thumb_name = f'thumb{message.message_id}.jpg'
+            thumb = requests.get(thumbnail, allow_redirects=True)
+            open(thumb_name, 'wb').write(thumb.content)
+            hash_object = hashlib.sha256()
+            hash_object.update(title.encode('utf-8'))
+            hash_value = hash_object.hexdigest()
+            keyw = hash_value[:5]
+
+        except Exception as e:
+            print(e)
+            await message.reply_text('**👎 Nothing found Retry with another !**')
+            return
+    except Exception as e:
+        await message.reply_text(
+            "**Enter Song Name with /song Command!**"
+        )
+        print(str(e))
+        return
+    m = await message.reply_text("<code>✨ Fetching... </code>")
+    
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+            AUDIO[keyw] = {
+                "audio_file": audio_file,
+                "title": title,
+                "duration": duration,
+                "link": link,
+                "thumb_name": thumb_name
+            }
+        rep = f'<a>{title}</a>\n\n❍ <b>Duration:</b> <code>{duration}</code>\n❍ <b>Uploaded By:</b> <a href="https://t.me/Edit_Repo">BenbotZ</a>\n<b>❍ Source:</b> <a href="{link}">Click Here</a>'
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr) - 1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        await message.reply_audio(
+            audio_file, caption=rep, parse_mode='HTML', quote=False, title=title, duration=dur, performer=performer,
+            thumb=thumb_name,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("✨ Send - Personally ✨", callback_data=f'sendpm#{keyw}')
+                    ]
+                ]
+            ),
+            reply_to_message_id=message.message_id
+        )
+        await m.delete()
+    except Exception as e:
+        await message.reply_text('**An internal Error Occured, Report This @Edit_repo !!**')
+        print(e)
+  
