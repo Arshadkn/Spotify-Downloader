0e5c918c31064cac94d7d9e5feb144ae6d79240f
Arshadkn <93204996+Arshadkn@users.noreply.github.com>
2023-08-01 22:10:12 +0530

    Update song.py

--- mbot/plugins/song.py
+++ mbot/plugins/song.py
@@ -91,4 +91,26 @@ async def song_fetch(client, message):
     except Exception as e:
         await message.reply_text('**An internal Error Occured, Report This @Edit_repo !!**')
         print(e)
-  
+
+@Client.on_callback_query(filters.regex(r"^sendpm"))
+async def callback_handler(client, query):
+    data = query.data
+    sng = data.split("#")[1]
+    audio_file = AUDIO[sng]["audio_file"]  
+    duration = AUDIO[sng]["duration"]
+    title = AUDIO[sng]["title"]    
+    link = AUDIO[sng]["link"]
+    performer = f"[@AnnabenbotZ]"
+    thumb_name = AUDIO[sng]["thumb_name"]
+    rep = f'<a>{title}</a>\n\n❍ <b>Duration:</b> <code>{duration}</code>\n❍ <b>Uploaded By:</b> <a href="https://t.me/Edit_Repo">BenbotZ</a>\n<b>❍ Source:</b> <a href="{link}">Click Here</a>'  
+    try:
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr) - 1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        user_id = query.from_user.id        
+        await client.send_audio(user_id, audio_file, caption=rep, parse_mode='HTML', title=title, duration=dur, performer=performer, thumb=thumb_name, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ɢʀᴏᴜᴘ ✨🌟", url="https://t.me/+BzleUoO-duFmODRl")]]))
+        await query.answer("Audio Send Successfully", show_alert=True)
+    except ChatWriteForbidden:
+        print("Cannot send a message to this user.")     
+        await query.answer("Start The Bot!", show_alert=True)
