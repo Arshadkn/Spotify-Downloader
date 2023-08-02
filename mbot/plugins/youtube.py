79748b8b07b7e84e77f34b12adb8cd7ba08b609e
Arshadkn <93204996+Arshadkn@users.noreply.github.com>
2023-08-02 20:57:24 +0530

    Update youtube.py

--- mbot/plugins/youtube.py
+++ mbot/plugins/youtube.py
@@ -89,11 +89,7 @@ async def callback_handler(client, query):
         print("Cannot send a message to this user.")     
         await query.answer("Start The Bot!", show_alert=True)
 
-    try:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(link, download=False)
-            audio_file = ydl.prepare_filename(info_dict)
-            ydl.process_info(info_dict)
+    
             AUDIO[keyw] = {
                 "audio_file": audio_file,
                 "title": title,
